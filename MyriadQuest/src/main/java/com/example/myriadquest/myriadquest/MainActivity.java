7099e5670b676d93eaa99cda75bb3f27dd528d47
Charles Fangsrud <charles.fangsrud@gmail.com>
2014-05-20 15:18:18 -0500

    Initial login button function

--- MyriadQuest/src/main/java/com/example/myriadquest/myriadquest/MainActivity.java
+++ MyriadQuest/src/main/java/com/example/myriadquest/myriadquest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,4 +36,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /** Verify that the given login details are valid. **/
+    public void verifyLogin(View view) {
+
+    }
 }
