b44a357058fa033a89475caee754b6f05c7c4179
Charles Fangsrud <charles.fangsrud@gmail.com>
2014-05-24 19:39:52 -0500

    Create Settings Activity

--- MyriadQuest/src/main/java/com/example/myriadquest/myriadquest/QuestListActivity.java
+++ MyriadQuest/src/main/java/com/example/myriadquest/myriadquest/QuestListActivity.java
@@ -36,6 +36,8 @@ public class QuestListActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
